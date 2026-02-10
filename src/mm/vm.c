3ad418360301abb605f5d8d47553c8eaa9f27752
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-02-10 14:45:28 -0600

    h

--- src/mm/vm.c
+++ src/mm/vm.c
@@ -2,6 +2,7 @@
 #include "paging.h"
 #include "pmm.h"
 #include "str.h"
+#include "test.h"
 #include <stdint.h>
 void init_paging() {
   uint32_t *page_directory = alloc_page(1); // must return a 4 KB aligned frame
