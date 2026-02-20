5927fa737fa12a6b259ee89ea88cd89a620f4181
Lucas Simpson <113938275+lucassimpson0213@users.noreply.github.com>
2026-02-20 01:15:09 -0600

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -15,15 +15,15 @@
 #include <stdint.h>
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-#error                                                                         \
-    "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
+//#if defined(__linux__)
+//#error                                                                         \
+ //   "You are not using a cross-compiler, you will most certainly run into trouble"
+//#endif
 
 /* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
+//#if !defined(__i386__)
+//#error "This tutorial needs to be compiled with a ix86-elf compiler"
+//#endif
 
 void terminal_scroll();
 /* Hardware text mode color constants. */
